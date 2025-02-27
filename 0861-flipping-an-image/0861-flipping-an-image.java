90096b84146aa60e8559cf7a005ae079c6da3e50
PUSHPENDRA PRAJAPAT <82139960+Pushpendra-09@users.noreply.github.com>
2025-02-27 16:08:39 +0530

    Time: 0 ms (100%), Space: 44.8 MB (39.37%) - LeetHub

--- 0861-flipping-an-image/0861-flipping-an-image.java
+++ 0861-flipping-an-image/0861-flipping-an-image.java
@@ -1,45 +1,11 @@
 class Solution {
     public int[][] flipAndInvertImage(int[][] image) {
         int n=image.length;
-        int m=image[0].length;
-       
-        for (int i = 0; i < n; i++) {
-  
-        // Initialise start and end index
-        int start = 0;
-        int end = n - 1;
-  
-        // Till start < end, swap the element
-        // at start and end index
-        while (start < end) {
-  
-            // Swap the element
-            int temp = image[i][start];
-            image[i][start] = image[i][end];
-            image[i][end] = temp;
-  
-            // Increment start and decrement
-            // end for next pair of swapping
-            start++;
-            end--;
-        }
-        }
-
-        for(int i=0;i<image.length;i++)
+        for(int[] row: image)
+        for(int i=0;i*2<n;i++)
         {
-            for(int j=0;j<m;j++)
-            {
-                if(image[i][j]==0){
-                    
-                    image[i][j]=1;
-                }
-            else{ 
-                if(image[i][j]==1)
-                {
-                    image[i][j]=0;
-                }
-            }
-            }
+            if(row[i]==row[n-i-1])
+                row[i]=row[n-i-1]^=1;
         }
         return image;
         
