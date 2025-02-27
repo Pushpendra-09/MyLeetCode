c4533566f46d4747bb37103fcf0b4f05260c8296
PUSHPENDRA PRAJAPAT <82139960+Pushpendra-09@users.noreply.github.com>
2025-02-27 11:23:41 +0530

    Time: 0 ms (100%), Space: 41.9 MB (48.5%) - LeetHub

--- 1505-create-target-array-in-the-given-order/1505-create-target-array-in-the-given-order.java
+++ 1505-create-target-array-in-the-given-order/1505-create-target-array-in-the-given-order.java
@@ -1,23 +1,16 @@
 class Solution {
     public int[] createTargetArray(int[] nums, int[] index) {
-        // Initialize a target array with the same length as nums
-        int[] target = new int[nums.length];
-        
-        // Fill the target array with a sentinel value (e.g., -1) to represent uninitialized values
-        for (int i = 0; i < nums.length; i++) {
-            target[i] = -1;  // You could also use another sentinel value based on the problem constraints
-        }
-
-        // Loop through each element in nums and insert it at the correct index in the target array
-        for (int i = 0; i < nums.length; i++) {
-            // Shift elements to the right if necessary to make space
-            for (int j = nums.length - 1; j > index[i]; j--) {
-                target[j] = target[j - 1];
-            }
-            // Insert the current element at the correct index
-            target[index[i]] = nums[i];
-        }
-
-        return target;
+       ArrayList<Integer>list=new ArrayList<>();
+       for(int i=0;i<nums.length;i++)
+       {
+        list.add(index[i],nums[i]);
+        //System.out.println(list.get(i));
+       }
+       int ans[]=new int[list.size()];
+       for(int i=0;i<list.size();i++)
+       {
+        ans[i]=list.get(i);
+       }
+       return ans;
     }
 }
