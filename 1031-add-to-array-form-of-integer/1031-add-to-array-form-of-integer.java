9083398caecead4646459983a62f5f5a71662699
PUSHPENDRA PRAJAPAT <82139960+Pushpendra-09@users.noreply.github.com>
2025-03-05 16:31:58 +0530

    Time: 2 ms (99.31%), Space: 45.9 MB (55.64%) - LeetHub

---
+++ 1031-add-to-array-form-of-integer/1031-add-to-array-form-of-integer.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<Integer> addToArrayForm(int[] num, int k) {
+         List<Integer> res = new LinkedList<>();
+         
+         int i=num.length-1;
+         while(i>=0|| k>0)
+         {
+            if(i>=0)
+            {
+                k+=num[i];
+                i--;
+            }
+            
+         
+         res.add(0,k%10);
+         k/=10;
+         }
+     
+      return res;  
+    }
+        
+}
